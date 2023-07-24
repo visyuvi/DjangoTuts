e735a678c90f4813aa62bddc59eb5806a1e0a0af
Your Name <you@example.com>
2023-07-24 11:50:45 +0530

    Setup  installation and page navigation

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+
+def index(response):
+    return HttpResponse("<h1>Spidey is back!</h1>")
+
+
+def v1(response):
+    return HttpResponse("<h1>View 1</h1>")
